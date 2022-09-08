5bdd97686782b43f0b366b8529392340a12c09ea
daffarizqanfairuz <112565811+daffarizqanfairuz@users.noreply.github.com>
2022-09-08 09:41:57 +0700

    Add files via upload

---
+++ Persegi.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int p,luas;
+	
+	cout<<"masukkan panjang sisi persegi: ";
+	cin>>p;
+
+	luas=p*p;
+	cout<<"luas persegi panjang adalah: "<<luas;
+
+	int x,keliling;
+	
+	cout<<"\nmasukkan panjang sisi persegi: ";
+	cin>>x;
+	
+	keliling=x*4;
+	cout<<"keliling persegi adalah: "<<keliling;
+	return 0;
+}
