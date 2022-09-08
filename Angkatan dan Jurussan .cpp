bf853ad2864604c79a02e7187bb9a27de759a606
daffarizqanfairuz <112565811+daffarizqanfairuz@users.noreply.github.com>
2022-09-08 09:50:04 +0700

    Add files via upload

---
+++ Angkatan dan Jurussan .cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main (){
+	string a;
+	cout <<"angkatan anda : " ;
+	getline(cin,a);
+	string b;
+	cout<<"jurusan anda : " ;
+	getline(cin,b);
+	
+	cout<<"\nngkatan anda adalah : "<<a;
+	cout<<"\njurusan anda adalah : "<<b;
+		return 0;
+}
